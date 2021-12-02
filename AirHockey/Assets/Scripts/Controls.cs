4c992769ef9cc1d0def4e14562951441a7fccfd9
WVincent77 <wvincent98949@gmail.com>
2021-12-01 20:24:25 -0500

    Game files added

---
+++ AirHockey/Assets/Scripts/Controls.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float force = 10.0f;
+    public float lBarrier = -1.7f;
+    public float rBarrier = 1.7f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (transform.position.x > lBarrier)
+        {
+            transform.position = new Vector3(lBarrier, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x < rBarrier)
+        {
+            transform.position = new Vector3(rBarrier, transform.position.y, transform.position.z);
+        }
+
+        while (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * force);
+        }
+    }
+}
---
+++ AirHockey/Assets/Scripts/Starter.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Starter : MonoBehaviour
+{
+    private Button button;
+    public GameObject titleScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        titleScreen.gameObject.SetActive(true);
+        button.onClick.AddListener(BeginGame);
+
+    }
+
+    public void BeginGame()
+    {
+        Debug.Log(gameObject.name + " was clicked");
+        titleScreen.gameObject.SetActive(false);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
